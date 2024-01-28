13c72df009a2b5434c575717715cc0368a940971
raul <2021171023@uteq.edu.mx>
2024-01-28 15:54:37 -0600

    cors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Cambia la URL de la API según tu configuración
+        const response = await axios.get('http://tu-api-externa.com/api/data');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error al realizar la solicitud:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {data ? `Data from API: ${JSON.stringify(data)}` : 'Loading...'}
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
